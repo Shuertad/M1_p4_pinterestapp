5e7579867d53b16322ff1e0805a8483e12a51663
shuertad <santibhs@yahoo.com>
2024-01-05 17:35:09 +0100

    correct error with eventListeners running before initial image load

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const init = async () => {
   renderHeader();
   await renderImages();
   renderFooter();
+
+  document.body.querySelector(".button-refresh-home").addEventListener('click',refreshButton);
+  document.body.querySelector(".search-bar").addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent the default behavior of the Enter key (e.g., form submission)
+        query(document.getElementById("query-input").value);
+    }
+  });
 }
 
-init()
-
-document.body.querySelector(".button-refresh-home").addEventListener('click',refreshButton);
-document.body.querySelector(".search-bar").addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-      event.preventDefault(); // Prevent the default behavior of the Enter key (e.g., form submission)
-      query(document.getElementById("query-input").value);
-  }
-});
+init();
